e00716a03791ae04fc50cf58efc8466d0ae31b4b
Dmytro Sych <dmytro.sych@evry.com>
2021-01-21 15:52:38 +0200

    Add iteraction on folders

--- InvertedIndex/InvertedIndex/FoldersParser.cs
+++ InvertedIndex/InvertedIndex/FoldersParser.cs
@@ -1,7 +1,8 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace InvertedIndex
@@ -9,9 +10,35 @@ namespace InvertedIndex
     public class FoldersParser
     {
         private readonly ConcurrentQueue<string> _filesToParse;
+        
         public FoldersParser(IEnumerable<string> folders)
         {
             _filesToParse = new ConcurrentQueue<string>(folders.SelectMany(Directory.GetFiles));
         }
-    }
+
+		public async Task<ConcurrentDictionary<string, ConcurrentBag<string>>> ProcessFiles(int tasksNumber)
+		{
+			Console.WriteLine($"{_filesToParse.Count} files to process");
+			var tasks = new List<Task>();
+			for (var i = 0; i < tasksNumber; i++)
+			{
+				tasks.Add(ParseTask(i));
+			}
+
+			await Task.WhenAll(tasks);
+
+			Console.WriteLine("All files processed");
+			return null;
+		}
+
+		private async Task ParseTask(int id)
+		{
+			while (!_filesToParse.IsEmpty)
+			{
+				_filesToParse.TryDequeue(out string fileToParse);
+				Console.WriteLine($"{id}: I took {fileToParse}");
+			}
+			Console.WriteLine($"{id}: Finished");
+		}
+	}
 }
--- InvertedIndex/InvertedIndex/Program.cs
+++ InvertedIndex/InvertedIndex/Program.cs
@@ -1,19 +1,22 @@
 ﻿using System;
+using System.Collections.Concurrent;
 
 namespace InvertedIndex
 {
     class Program
     {
-        static void Main(string[] args)
+        private static int _numberOfTasks = 4;
+        static async System.Threading.Tasks.Task Main(string[] args)
         {
             var FoldersParser = new FoldersParser(new[]
             {
-                "..\\..\\..\\1",
-                "..\\..\\..\\2",
-                "..\\..\\..\\3",
-                "..\\..\\..\\4",
-                "..\\..\\..\\5",
+                "D:\\Univer\\parallel\\course\\1",
+                "D:\\Univer\\parallel\\course\\2",
+                "D:\\Univer\\parallel\\course\\3",
+                "D:\\Univer\\parallel\\course\\4",
+                "D:\\Univer\\parallel\\course\\5",
             });
+            ConcurrentDictionary<string, ConcurrentBag<string>> index = await FoldersParser.ProcessFiles(_numberOfTasks);
         }
     }
 }
